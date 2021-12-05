01d8826183a79b8e5edbb78339cc0fec6b36263b
Arno Cellarier <mrfeyurikenpro@gmail.com>
2021-12-05 20:48:49 +0100

    update font params

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,6 +5,7 @@
       <p class="ac-pane__description">
         Bonjour, actuellement étudiant en BUT informatique, je conçois des sites internet.
       </p>
+      <span class="ac-pane__arrow">↓</span>
     </div>
     <div class="ac-pane ac-pane--white"></div>
     <div class="ac-pane ac-pane--black"></div>
