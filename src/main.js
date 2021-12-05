27a26cce12e110bda81c5fe6c8803ff9ea0a68d4
Arno Cellarier <mrfeyurikenpro@gmail.com>
2021-12-05 16:11:51 +0100

    add custom font and scss

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 
+import '~/assets/styles/styles.scss'
+
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,20 @@
 <template>
-  <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-  </Layout>
+  <div>
+    <div class="ac-pane ac-pane--grey ac-pane--box">
+      <h1 class="ac-pane__title">Arno Cellarier</h1>
+      <p class="ac-pane__description">
+        Bonjour, actuellement étudiant en BUT informatique, je conçois des sites internet.
+      </p>
+    </div>
+    <div class="ac-pane ac-pane--white"></div>
+    <div class="ac-pane ac-pane--black"></div>
+  </div>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Bonjour !'
   }
 }
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
