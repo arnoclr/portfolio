1402c23620d053b26a17f2320b7364c0025e06c2
Arno Cellarier <mrfeyurikenpro@gmail.com>
2022-01-15 10:16:17 +0100

    add image optimization

--- src/app.js
+++ src/app.js
@@ -7,3 +7,32 @@ if (navigator.language.includes('fr')) {
     t.textContent = t.getAttribute('fr');
   });
 }
+
+// lazy load images
+var observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0.0) {
+        img = entry.target;
+        if (!img.hasAttribute('loaded')) {
+          // find the noscript tag just below the image
+          const noscript = img.nextElementSibling;
+
+          // find image source
+          const HTML = noscript.innerText;
+          const src = HTML.match(/src="(.*?)"/)[1];
+
+          img.setAttribute('src', src);
+          img.setAttribute('loaded', true);
+        }
+      }
+    });
+  },
+  {}
+)
+
+const images = document.querySelectorAll('.js-lazy');
+
+images.forEach(img => {
+  observer.observe(img);
+});
