ba3690f496769b9c14e1e8e40650a27d6634c313
Arno Cellarier <mrfeyurikenpro@gmail.com>
2022-01-21 18:19:25 +0100

    remove fr attribute after apply of translation

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const translatables = document.querySelectorAll('[fr]');
 if (navigator.language.includes('fr')) {
   translatables.forEach(t => {
     t.textContent = t.getAttribute('fr');
+    // remove the attribute
+    t.removeAttribute('fr');
   });
 }
 
